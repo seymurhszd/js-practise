ec60bbf3b184b7628aaae744bab22ee908f62ccf
seymurhszd <seymurrm@gmail.com>
2023-04-16 16:31:23 +0400

    task6

---
+++ task#6/main.js
@@ -0,0 +1,17 @@
+function showMessage() {
+    let name = prompt('Ваше имя:')
+    let age = prompt ('Ваш возраст:')
+    let numberOfOrders = prompt ('Количество ваших заказов:')
+    let amountOfOrders = prompt ('Введите сумму ваших заказов:')
+
+    if (age >= 60) {
+        alert ('Вам пологается скидка!')
+    }   else if  (numberOfOrders >= 4 && amountOfOrders >= 100) {
+        alert ('Вам пологается скидка!')
+    }
+        else {
+            alert ('Спасибо за вашу информацию!')
+        }
+        
+}
+showMessage();
