0ae15024505e63ccaa10cae03501336a21b295be
seymurhszd <seymurrm@gmail.com>
2023-04-16 17:57:50 +0400

    task7

---
+++ task#7/main.js
@@ -0,0 +1,37 @@
+function showMessage() {
+  let length = prompt("Введите длину товара:");
+  let width = prompt("Введите ширину товара:");
+  let height = prompt("Введите высоту товара:");
+  let weight = prompt("Введите вес товара:");
+  let distance = prompt("Введите километраж до вашего адреса:");
+
+  // if (length + width + height <= 150 && length < 100 && width < 100 && height < 100 && weight <= 10 && distance >=3 && distance <=10) {
+  //     alert('Отправляемый вами товар является малогабаритным');
+  // } else {
+  //     alert('Ваш товар крупно габаритный');
+  // }
+
+//   if (length + width + height <= 150) {
+//     if (length <= 100 && width <= 100 && height <= 100) {
+//       if (weight <= 10) {
+//         if (distance >= 3 && distance <= 10) {
+//           alert("melkaya posilka");
+//         } else {
+//           alert("Ваша посылка не подходит по дистанции");
+//         }
+//       } else {
+//         alert("Ваша посылка не подходит по весу");
+//       }
+//     } else {
+//       alert("Ваша посылка не подхолит по размерам");
+//     }
+//   } else {
+//     alert("Ваша посылка не подхолит по размерам");
+//   }
+// }
+
+
+
+// В чем ошибка?((((
+}
+showMessage();
